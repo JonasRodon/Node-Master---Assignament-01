7bf2391647674aff61c250cf34e58f6cc24ac09e
Jonas Rodon <jonasrodon@gmail.com>
2018-10-29 11:21:14 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,124 @@
+/*
+ * Primary file for the API
+ *
+ *
+ */
+
+// Dependencies
+var http = require('http');
+var https = require('https');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
+var fs = require('fs');
+
+// Instanti#ate de HTTP server
+var httpServer = http.createServer(function (req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the HTTP server
+
+httpServer.listen(config.httpPort, function () {
+    console.log('The server is listenig in por ' + config.httpPort);
+});
+
+// Instanti#ate de HTTPS server
+var httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/certificado.pem')
+};
+var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, function () {
+    console.log('The server is listenig in por ' + config.httpsPort);
+});
+
+// All the server logic for both the http and the https server
+var unifiedServer = function (req, res) {
+    // Get the URL and parse it
+    var parsedUrl = url.parse(req.url, true);
+
+    // Get the path 
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query strig as an Object
+    var queryStringObject = parsedUrl.query;
+
+    // Get the HTTP Method
+    var method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    var headers = req.headers;
+
+    // Get the payload if any
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function (data) {
+        buffer += decoder.write(data)
+    });
+    req.on('end', function () {
+        buffer += decoder.end();
+
+        // Chose the handler were should go to. If One is not found, use the notFound handler
+        var chosenHandler = typeof (router[trimmedPath]) != 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        // Construct the data object to send to the handler
+        var data = {
+            'trimmedPath': trimmedPath,
+            'querySrtingObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
+
+        // Route the request to the handler specified in the router
+        chosenHandler(data, function (statusCode, payload) {
+
+            // Use the status code called back by the handler, or default 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload called back by the handler, or default to an emty object
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            // Convert a payload to string
+            var payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.setHeader('Content-Type', 'aplication/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // Log the request path
+            console.log('Returning this response: ', statusCode, payloadString);
+        });
+    });
+
+};
+// Define the handlers
+var handlers = {};
+
+// Ping handler
+handlers.ping = function(data,callback){
+    callback(200);
+};
+
+// Hello handler
+handlers.hello = function(data,callback){
+    callback(200, {'hello': 'Welcome to home assignment #1 REST API server made by Jonas Rodon 🔥 '});
+};
+
+// Not found handler
+handlers.notFound = function (data, callback) {
+    callback(404);
+};
+
+// Define a request router
+var router = {
+    'ping': handlers.ping,
+    'hello': handlers.hello
+};
