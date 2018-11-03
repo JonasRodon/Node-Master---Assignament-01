ae2c298e9dc9210b3ca18b838751c4611f37bd0b
Jonas Rodon <jonasrodon@gmail.com>
2018-11-03 20:37:00 +0000

    Fix index to process get request

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /*
- * Primary file for the API
- *
+ * Primary file for API
  *
  */
 
@@ -11,103 +10,97 @@ var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./lib/config');
 var fs = require('fs');
-var _data = require('./lib/data');
 var handlers = require('./lib/handlers');
 var helpers = require('./lib/helpers');
 
-// Instanti#ate de HTTP server
-var httpServer = http.createServer(function (req, res) {
-    unifiedServer(req, res);
+ // Instantiate the HTTP server
+var httpServer = http.createServer(function(req,res){
+  unifiedServer(req,res);
 });
 
 // Start the HTTP server
-
-httpServer.listen(config.httpPort, function () {
-    console.log('The server is listenig in por ' + config.httpPort);
+httpServer.listen(config.httpPort,function(){
+  console.log('The HTTP server is running on port '+config.httpPort);
 });
 
-// Instanti#ate de HTTPS server
+// Instantiate the HTTPS server
 var httpsServerOptions = {
-    'key': fs.readFileSync('./https/key.pem'),
-    'cert': fs.readFileSync('./https/certificado.pem')
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/certificado.pem')
 };
-var httpsServer = https.createServer(httpsServerOptions, function (req, res) {
-    unifiedServer(req, res);
+var httpsServer = https.createServer(httpsServerOptions,function(req,res){
+  unifiedServer(req,res);
 });
 
 // Start the HTTPS server
-httpsServer.listen(config.httpsPort, function () {
-    console.log('The server is listenig in por ' + config.httpsPort);
+httpsServer.listen(config.httpsPort,function(){
+ console.log('The HTTPS server is running on port '+config.httpsPort);
 });
 
-// All the server logic for both the http and the https server
-var unifiedServer = function (req, res) {
-    // Get the URL and parse it
-    var parsedUrl = url.parse(req.url, true);
+// All the server logic for both the http and https server
+var unifiedServer = function(req,res){
 
-    // Get the path 
-    var path = parsedUrl.pathname;
-    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  // Parse the url
+  var parsedUrl = url.parse(req.url, true);
 
-    // Get the query strig as an Object
-    var queryStringObject = parsedUrl.query;
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-    // Get the HTTP Method
-    var method = req.method.toLowerCase();
+  // Get the query string as an object
+  var queryStringObject = parsedUrl.query;
 
-    // Get the headers as an object
-    var headers = req.headers;
+  // Get the HTTP method
+  var method = req.method.toLowerCase();
 
-    // Get the payload if any
-    var decoder = new StringDecoder('utf-8');
-    var buffer = '';
-    req.on('data', function (data) {
-        buffer += decoder.write(data)
-    });
-    req.on('end', function () {
-        buffer += decoder.end();
+  //Get the headers as an object
+  var headers = req.headers;
 
-        // Chose the handler were should go to. If One is not found, use the notFound handler
-        var chosenHandler = typeof (router[trimmedPath]) != 'undefined' ? router[trimmedPath] : handlers.notFound;
+  // Get the payload,if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function(data) {
+      buffer += decoder.write(data);
+  });
+  req.on('end', function() {
+      buffer += decoder.end();
 
-        // Construct the data object to send to the handler
-        var data = {
-            'trimmedPath': trimmedPath,
-            'querySrtingObject': queryStringObject,
-            'method': method,
-            'headers': headers,
-            'payload': helpers.parseJsonToObject(buffer)
-        };
+      // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
+      var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-        // Route the request to the handler specified in the router
-        chosenHandler(data, function (statusCode, payload) {
+      // Construct the data object to send to the handler
+      var data = {
+        'trimmedPath' : trimmedPath,
+        'queryStringObject' : queryStringObject,
+        'method' : method,
+        'headers' : headers,
+        'payload' : helpers.parseJsonToObject(buffer)
+      };
 
-            // Use the status code called back by the handler, or default 200
-            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+      // Route the request to the handler specified in the router
+      chosenHandler(data,function(statusCode,payload){
 
-            // Use the payload called back by the handler, or default to an emty object
-            payload = typeof (payload) == 'object' ? payload : {};
+        // Use the status code returned from the handler, or set the default status code to 200
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
 
-            // Convert a payload to string
-            var payloadString = JSON.stringify(payload);
+        // Use the payload returned from the handler, or set the default payload to an empty object
+        payload = typeof(payload) == 'object'? payload : {};
 
-            // Return the response
-            res.setHeader('Content-Type', 'aplication/json');
-            res.writeHead(statusCode);
-            res.end(payloadString);
+        // Convert the payload to a string
+        var payloadString = JSON.stringify(payload);
 
-            // Log the request path
-            console.log('Returning this response: ', statusCode, payloadString);
-        });
-    });
+        // Return the response
+        res.setHeader('Content-Type', 'application/json');
+        res.writeHead(statusCode);
+        res.end(payloadString);
+        console.log(trimmedPath,statusCode);
+      });
 
+  });
 };
 
-
-// Define a request router
+// Define the request router
 var router = {
-    'ping': handlers.ping,
-    'hello': handlers.hello,
-    'users': handlers.users
-
+  'ping' : handlers.ping,
+  'users' : handlers.users
 };
