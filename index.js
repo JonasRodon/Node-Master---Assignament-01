fc2844739276a18fb24ec81da4ea00daa6894dd6
Jonas Rodon <jonasrodon@gmail.com>
2018-11-16 12:38:59 +0000

    Adding API Twilio suport

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ var fs = require('fs');
 var handlers = require('./lib/handlers');
 var helpers = require('./lib/helpers');
 
+// @TODO GET RID OF THIS
+helpers.sendTwilioSms('4155835309','Hello!', function (err) {
+  console.log('This was the error',err);
+});
+
  // Instantiate the HTTP server
 var httpServer = http.createServer(function(req,res){
   unifiedServer(req,res);
